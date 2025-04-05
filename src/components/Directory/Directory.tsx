394eff5402b59692904e79b2b364c8fdd96823b1
Andy J Phillips <74455172+andy-j-phillips@users.noreply.github.com>
2025-04-05 16:04:37 -0400

    0012 - Improve Directory styles (#6)

--- src/components/Directory/Directory.tsx
+++ src/components/Directory/Directory.tsx
@@ -1,11 +1,14 @@
 import ListItem from '../ListItem';
+import ListHeaderItem from '../ListItem/ListHeaderItem';
 import directorySelector from '../../store/selectors/directorySelector';
 import styles from './Directory.module.scss';
 
 const Directory = () => {
   const directory = directorySelector();
+
   return (
     <ul className={styles.directory}>
+      <ListHeaderItem />
       {directory.children.map((item) => (
         <ListItem key={item.id} item={item} />
       ))}
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,7 +1,9 @@
-import { FileManagerItem, DisplayColumns } from '../../types';
+import { FileManagerItem } from '../../types';
 import { useDispatch } from '../../store/dispatch';
 import styles from './ListItem.module.scss';
 import displayColumnsSelector from '../../store/selectors/displayColumnsSelector';
+import folderIcon from '../../assets/icons/folder.svg';
+import fileIcon from '../../assets/icons/file.svg';
 
 type ListItemProps = {
   item: FileManagerItem;
@@ -20,8 +22,15 @@ const ListItem = ({ item }: ListItemProps) => {
   };
 
   return (
-    <li className={styles.listItem} onDoubleClick={handleDoubleClick}>
-      <div>{item.name}</div>
+    <li className={styles.listItemBody} onDoubleClick={handleDoubleClick}>
+      <div>
+        <img
+          src={item.type === 'file' ? fileIcon : folderIcon}
+          alt="Folder Icon"
+          className={styles.listItemBodyIcon}
+        />{' '}
+        {item.name}
+      </div>
       {displayColumns.map((column) => (
         // TODO Fix any here
         <div key={column}>{item.hasOwnProperty(column) && (item as any)[column]}</div>
